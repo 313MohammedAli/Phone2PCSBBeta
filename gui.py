dc1688574870d1774822263989a655181eac0642
MrTuxy <mrtuxy2@gmail.com>
2024-02-07 19:14:10 -0500

    Chad test

--- gui.py
+++ gui.py
@@ -10,7 +10,7 @@ ctk.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 ctk.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 print("gui.py started")
 
-
+#test
 
 class MessageApp(customtkinter.CTk):
     def __init__(self, q):
